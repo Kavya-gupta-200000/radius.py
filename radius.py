b4bd78c721ffd80d8f3208e3e5827c5719c396c2
Kavya-gupta-200000 <90207911+Kavya-gupta-200000@users.noreply.github.com>
2021-09-20 00:06:05 -0700

    developed by kavya

---
+++ radius.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 19 23:42:22 2021
+
+@author: kcs
+"""
+
+from math import pi
+r = float(input("Input the radius of the circle : "))
+print("The area of the circle with radius" + str(radius) + "is:" + str(pi * r **2))
